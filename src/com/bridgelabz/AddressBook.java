83413fa8fe892bf5eb6b9934fa6ece3e424363bf
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-22 09:12:19 +0530

    welcome

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*****WELCOME TO ADDRESS BOOK***");
+
+	}
+
+}
